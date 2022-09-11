cc2a6302164f3c3928bb34b7c4f62a352c4cd6dc
bajayi-obe <benjamin.ajayi-obe@depop.com>
2022-09-11 10:50:01 +0200

    create unit test for weighted accuracy

--- tests/metrics/numpy/classification/test_numpy_accuracy.py
+++ tests/metrics/numpy/classification/test_numpy_accuracy.py
@@ -8,3 +8,15 @@ import pytest
 def test_base_accuracy(accuracy, y_observed, y_predicted, expected):
     actual = accuracy(y_observed=y_observed, y_predicted=y_predicted)
     assert expected == actual
+
+
+@pytest.mark.usefixtures("accuracy")
+@pytest.mark.parametrize(
+    "y_observed,y_predicted,sample_weights,expected",
+    [([1, 1, 0, 1], [1, 0, 0, 1], [0.25, 0.25, 0.25, 0.25], 0.75)],
+)
+def test_weighted_accuracy(accuracy, y_observed, y_predicted, sample_weights, expected):
+    actual = accuracy(
+        y_observed=y_observed, y_predicted=y_predicted, sample_weights=sample_weights
+    )
+    assert expected == actual
