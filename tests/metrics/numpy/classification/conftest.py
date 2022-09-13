b5bff820b8dbc8c01da7d81da74f9eb2c3d44eb0
bajayi-obe <benjamin.ajayi-obe@depop.com>
2022-09-13 10:26:00 +0200

    updated tests with bool cases

--- tests/metrics/numpy/classification/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from otito.metrics.numpy.classification.accuracy import Accuracy
-
-
-@pytest.fixture
-def accuracy():
-    return Accuracy()
--- tests/metrics/numpy/classification/test_numpy_accuracy.py
+++ tests/metrics/numpy/classification/test_numpy_accuracy.py
@@ -1,32 +1,62 @@
 import pytest
+from otito.metrics.numpy import Accuracy
 
 
-@pytest.mark.usefixtures("accuracy")
-@pytest.mark.parametrize(
-    "y,y_predicted,expected",
-    [
-        ([0, 0, 0, 0], [1, 1, 1, 1], 0),
-        ([1, 1, 1, 1], [0, 0, 0, 0], 0),
-        ([1, 1, 1, 1], [1, 1, 1, 1], 1),
-        ([0, 0, 0, 0], [0, 0, 0, 0], 1),
-        ([1, 1, 0, 1], [1, 0, 0, 1], 0.75),
-    ],
-)
-def test_base_accuracy(accuracy, y, y_predicted, expected):
-    actual = accuracy(y_observed=y, y_predicted=y_predicted)
-    assert expected == actual
+class TestAccuracy:
+    """
+    Class to test Numpy Accuracy Metric Function
+    """
 
+    @pytest.fixture
+    def accuracy(self):
+        return Accuracy()
 
-@pytest.mark.usefixtures("accuracy")
-@pytest.mark.parametrize(
-    "y,y_predicted,sample_weights,expected",
-    [
-        ([1, 1, 0, 1], [1, 0, 0, 1], [0.25, 0.25, 0.25, 0.25], 0.75),
-        ([0, 0, 0, 0], [1, 1, 1, 1], [0.25, 0.25, 0.25, 0.25], 0.0),
-    ],
-)
-def test_weighted_accuracy(accuracy, y, y_predicted, sample_weights, expected):
-    actual = accuracy(
-        y_observed=y, y_predicted=y_predicted, sample_weights=sample_weights
+    @pytest.mark.usefixtures("accuracy")
+    @pytest.mark.parametrize(
+        "y_observed,y_predicted,expected",
+        [
+            ([0, 0, 0, 0], [1, 1, 1, 1], 0),
+            ([1, 1, 1, 1], [0, 0, 0, 0], 0),
+            ([1, 1, 1, 1], [1, 1, 1, 1], 1),
+            ([0, 0, 0, 0], [0, 0, 0, 0], 1),
+            ([1, 1, 0, 1], [1, 0, 0, 1], 0.75),
+            ([False, False, False, False], [True, True, True, True], 0),
+            ([True, True, True, True], [False, False, False, False], 0),
+            ([True, True, True, True], [True, True, True, True], 1),
+            ([False, False, False, False], [False, False, False, False], 1),
+            ([True, True, False, True], [True, False, False, True], 0.75),
+        ],
     )
-    assert expected == actual
+    def test_base_accuracy(self, accuracy, y_observed, y_predicted, expected):
+        actual = accuracy(y_observed=y_observed, y_predicted=y_predicted)
+        assert expected == pytest.approx(actual)
+
+    @pytest.mark.usefixtures("accuracy")
+    @pytest.mark.parametrize(
+        "y_observed,y_predicted,sample_weights,expected",
+        [
+            ([1, 1, 0, 1], [1, 0, 0, 1], [0.25, 0.25, 0.25, 0.25], 0.75),
+            ([0, 0, 0, 0], [1, 1, 1, 1], [0.25, 0.25, 0.25, 0.25], 0.0),
+            (
+                [True, True, False, True],
+                [True, False, False, True],
+                [0.25, 0.25, 0.25, 0.25],
+                0.75,
+            ),
+            (
+                [False, False, False, False],
+                [True, True, True, True],
+                [0.25, 0.25, 0.25, 0.25],
+                0.0,
+            ),
+        ],
+    )
+    def test_weighted_accuracy(
+        self, accuracy, y_observed, y_predicted, sample_weights, expected
+    ):
+        actual = accuracy(
+            y_observed=y_observed,
+            y_predicted=y_predicted,
+            sample_weights=sample_weights,
+        )
+        assert expected == pytest.approx(actual)
