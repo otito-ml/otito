b5142d93b511e666ee6e3e3649f80872869c60fb
bajayi-obe <benobe22@gmail.com>
2022-10-05 15:41:56 +0200

    add sphinx config

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here.
+import pathlib
+import sys
+
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "otito"
+copyright = "2022, Benjamin Ajayi-Obe"
+author = "Benjamin Ajayi-Obe"
+release = "0.0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.duration", "sphinx.ext.autodoc", "sphinx.ext.autosummary"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+autosummary_generate = True
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
---
+++ docs/source/index.rst
@@ -0,0 +1,25 @@
+.. otito documentation master file, created by
+   sphinx-quickstart on Mon Oct  3 16:39:07 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Otito's documentation!
+===================================
+
+**Otito** is a Python library that enables deep interrogation of ml models.
+
+Check out the :doc:`usage` section for further information. There you'll find information on how to :ref:`install <installation>` the package and also how to :ref:`get started <quickstart>`.
+
+
+.. note::
+
+   This project is under active development.
+
+Contents
+--------
+
+.. toctree::
+    :maxdepth: 3
+
+    usage
+    api
